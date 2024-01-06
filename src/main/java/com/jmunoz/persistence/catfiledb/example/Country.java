e09b78d61e943049002ee1d6c9231f2b63420fcc
Jorge Antonio Munoz <jorgemunozherbas@gmail.com>
2024-01-06 12:53:31 -0400

    test:lombok getter and setter - new test objects country and employee

--- src/main/java/com/jmunoz/persistence/catfiledb/example/Country.java
+++ src/main/java/com/jmunoz/persistence/catfiledb/example/Country.java
@@ -1,4 +1,23 @@
 package com.jmunoz.persistence.catfiledb.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
 public class Country {
+
+    private String name;
+    private String nameCode;
+    private String currencyCode;
+    private String capital;
+    private double area;
+    private Date dateCreation;
+    private int quantityStates;
+    private boolean isPartOfUN;
+    private double centerLatitude;
+    private double centerLongitude;
+    private int phoneCode;
 }
--- src/main/java/com/jmunoz/persistence/catfiledb/example/Employee.java
+++ src/main/java/com/jmunoz/persistence/catfiledb/example/Employee.java
@@ -1,5 +1,19 @@
 package com.jmunoz.persistence.catfiledb.example;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Employee {
+
+    private String code;
+    private String name;
+    private int area;
+    private double salary;
+    private boolean married;
+    private String gender;
+    private String documentID;
+    private String position;
 
 }
